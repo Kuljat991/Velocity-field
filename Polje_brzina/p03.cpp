ed003eebda4e1385055ef8233b25236bfc290a87
kuljat991 <kuljat@riteh.hr>
2018-07-02 13:41:34 +0200

    final version

---
+++ Polje_brzina/p03.cpp
@@ -0,0 +1,27 @@
+#include <QDebug>
+#include <QVector>
+#include <QVector2D>
+#include "pathconfig.h"
+#include "velocity_functions.h"
+#include "ThreadPoint.h"
+#include "utility.h"
+
+
+void drugitest()
+{
+
+}
+
+
+
+int main()
+{
+
+    //prvitest();
+    std::cout<<"**************\n";
+    drugitest();
+    std::cout<<"**************\n";
+
+
+    return 0;
+}
